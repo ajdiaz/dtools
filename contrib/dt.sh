d64ba8c99cb34ff93093ddaf338f963f6286613b
Andres J. Diaz <ajdiaz@connectical.com>
2011-02-24 18:16:23 +0100

    Add a set of utilities for shell.
    These are a number of utilities (functions and aliases) to be sourced from
    shell command line.

---
+++ contrib/dt.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+# Distributed Tools - dtools - contrib/dt.sh
+# Copyright (C) 2008 Andrés J. Díaz <ajdiaz@connectical.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+# Set of utilities to be sourced from bash shell.
+
+dtx () {
+	dt -N "$@" | dtcompact
+}
+
+dth () {
+	dt -N "$@" | dthost
+}
+
+dts () {
+	dt -N "$@" | dtstatus
+}
+
+alias dti="dt -i"
+alias dt100="dt -C 100"
+
